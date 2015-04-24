57c3bb6acb7f15e6f87d33c414fe94de2d5e640c
KamronSoldozy <2018ksoldozy@tjhsst.edu>
2015-04-24 19:18:17 -0400

    Create JSContact.js
    Java Script Contact form info, from KSLC repository (Kamron Soldozy Lawn Care)

---
+++ JSContact.js
@@ -0,0 +1,13 @@
+(function(d){
+	  var l = d.getElementById("foxyform_embed_link_660854");
+	      i = d.createElement('IFRAME');
+	  i.src = "http://www.foxyform.com/form.php?id=660854&sec_hash=4b1e432f286";
+	  i.width = "600px";
+	  i.height = "427px";
+	  i.frameborder = 0;
+	  i.setAttribute('style', 'border: none;');
+	  i.setAttribute('frameborder', '0');
+	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+	  l.parentNode.insertBefore(i,l);
+	})(document);
+		
